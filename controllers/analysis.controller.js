069b7374bcd8bd70913dd7f2bddd29de28271168
Muhammad Shayan <shayanarif666@gmail.com>
2025-09-18 16:04:25 +0500

    Server Side

--- controllers/analysis.controller.js
+++ controllers/analysis.controller.js
@@ -2,6 +2,7 @@ const Aircraft = require("../models/Aircraft.model");
 const Team = require("../models/Team.model");
 const Review = require("../models/Review.model");
 const Blog = require("../models/Blog.model");
+const Brand = require("../models/Brands.model");
 
 const getAnalysisLists = async (req, res) => {
   try {
@@ -9,11 +10,13 @@ const getAnalysisLists = async (req, res) => {
     const teams = await Team.find();
     const reviews = await Review.find();
     const blogs = await Blog.find();
+    const brands = await Brand.find();
 
     const aircraftCount = aircrafts.length;
     const teamCount = teams.length;
     const reviewCount = reviews.length;
     const blogCount = blogs.length;
+    const brandCount = brands.length;
 
     return res.json({
       data: {
@@ -21,6 +24,7 @@ const getAnalysisLists = async (req, res) => {
         team: teamCount,
         review: reviewCount,
         blog: blogCount,
+        brand: brandCount,
       },
       success: true,
       message: "Analysis found",
